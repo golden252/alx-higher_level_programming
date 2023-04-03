0fe25b4bbc8037a33fba4bd231c3ffed74599cd9
golden252 <gideonoyejide5@gmail.com>
2023-04-03 16:57:21 -0700

    0x08-python-more_classes project

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Defines a Recangle class'''
+
+
+class Rectangle:
+    '''
+    The Rectangle class represents a rectangle object.
+    Attributes:
+        width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+    Methods:
+        area(): Returns the area of the rectangle.
+        perimeter(): Returns the perimeter of the rectangle.
+    '''
+    pass
